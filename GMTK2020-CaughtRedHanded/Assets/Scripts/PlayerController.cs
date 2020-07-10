6f689780298c1ac4b40f6b617b7e80f77de7d97b
ceje <cdone245@gmail.com>
2020-07-10 10:20:40 -0500

    Add basic Player controller

---
+++ GMTK2020-CaughtRedHanded/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 m_Move;
+
+    private Vector2 m_Look;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    
+    //example functions from the input system controls.
+    //the PlayerInputs object handles the mapping of the keys and calls these functions with the proper inputs
+    public void OnMove(InputValue value)
+    {
+        m_Move = value.Get<Vector2>();
+        //probably set vector
+    }
+ 
+    public void OnLook(InputValue value)
+    {
+        m_Look = value.Get<Vector2>();
+        //probably adjust relative camera location and or viewcone of player if it exists
+    }
+ 
+    public void OnFire()
+    {
+        //do something
+    }
+}

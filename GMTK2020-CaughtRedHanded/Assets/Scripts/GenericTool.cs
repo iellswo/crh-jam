a50e92f15663adad5b857ad78630bf4ac34295e3
s15050 <45424170+s15050@users.noreply.github.com>
2020-07-11 02:17:06 +0200

    Added Generic Tool and toolbar
    MainScene now contains a toolbar and two GenericTool objects; Game Prefabs folder contains the GenericTool prefab. GenericTools are controlled with LMB; click on them to pick them up, click on the toolbar to put them down, click on the screen to use them (the last one has no effect so far; the method is intended for VFX and SFX).

---
+++ GMTK2020-CaughtRedHanded/Assets/Scripts/GenericTool.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericTool : MonoBehaviour
+{
+    public float mouseOffset = -30f; //Offset from cursor
+    public string toolName; //Name of the tool
+    public ToolTray toolTray; //Tool bar
+    public int slot; //Tool slot occupied by this tool
+
+    //Audio clips
+    public AudioClip pickUpSound;
+    public AudioClip putDownSound;
+    public AudioClip useSound;
+
+    private AudioSource audioPlayer;
+    private ParticleSystem particleSys;
+
+    private bool isPickedUp; //Whether the object is currently held by the player
+    private int currentClickState; //What the LMB will do to the object when clicked; see CommandSwitch method for explanation
+    private Vector3 returnPosition; //What the tool will return to after being put down
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = GetComponent<AudioSource>();
+        particleSys = GetComponent<ParticleSystem>();
+        particleSys.Stop();
+        isPickedUp = false;
+        currentClickState = 1;
+    }
+
+    private void Update()
+    {
+
+        if (isPickedUp)
+        {
+            //Following
+            Vector3 offset = new Vector3(mouseOffset, 0);
+            Vector3 mousePos = Input.mousePosition;
+            transform.position = mousePos + offset;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnClickReaction();
+            }
+        }
+    }
+
+    /// <summary>
+    /// Reaction to being clicked, based on 
+    /// </summary>
+    public void OnClickReaction()
+    {
+        switch (currentClickState)
+        {
+            case 1: PickUpTool(); break;
+            case 2: StartUsingTool(); break;
+            case 3: PutDownTool(); break;
+            default: Debug.Log("Incorrect clickState"); break;
+        }
+    }
+
+    /// <summary>
+    /// "Picks up" the tool, playing the audio and freeing it to follow the cursor; hides the cursor
+    /// </summary>
+    public void PickUpTool()
+    {
+        if (isPickedUp == false)
+        {
+            //audioPlayer.PlayOneShot(pickUpSound);
+            isPickedUp = true;
+            CommandSwitch(3);
+            toolTray.SetHeldTool(this);
+        }
+    }
+
+    /// <summary>
+    /// "Puts down" tool, playing audio and disengaging it from the cursor; makes the cursor visible again
+    /// </summary>
+    public void PutDownTool()
+    {
+        //audioPlayer.PlayOneShot(putDownSound);
+        isPickedUp = false;
+        CommandSwitch(1);
+        toolTray.UnsetHeldTool();
+        SnapToPosition();
+    }
+
+    /// <summary>
+    /// Starts playing the audio and particle effects associated with using the tool
+    /// </summary>
+    public void StartUsingTool()
+    {
+        //audioPlayer.PlayOneShot(useSound);
+        particleSys.Play();
+    }
+
+    /// <summary>
+    /// Stops playing the audio and particle effects associated with using the tool
+    /// </summary>
+    public void StopUsingTool()
+    {
+        //audioPlayer.Stop();
+        particleSys.Stop();
+    }
+
+    /// <summary>
+    /// Passes the tool's name (for interactions with problems)
+    /// </summary>
+    /// <returns></returns>
+    public string GetToolName()
+    {
+        return toolName;
+    }
+
+    /// <summary>
+    /// For interactions with ToolTray and clicking; pressing LMB at different points does different things
+    /// </summary>
+    /// <param name="command">1 for "to be picked up", 2 for "to be used", 3 for "to be put down"</param>
+    /// <returns></returns>
+    public void CommandSwitch(int command)
+    {
+        currentClickState = command;
+    }
+
+    /// <summary>
+    /// Snaps the item back to its position on the tool bar
+    /// </summary>
+    public void SnapToPosition()
+    {
+        Debug.Log("slot: " + slot + "; snap lock: " + toolTray.GetSnapLock(slot) + "; return x: " + returnPosition.x);
+        GetComponent<RectTransform>().localPosition = returnPosition;
+    }
+
+    /// <summary>
+    /// Sets the return position (scales with screen size)
+    /// </summary>
+    /// <param name="xfactor">Position of object on the X axis</param>
+    public void SetReturnPosition(float xfactor)
+    {
+        returnPosition = new Vector3(xfactor, 0);
+        SnapToPosition();
+    }
+}
---
+++ GMTK2020-CaughtRedHanded/Assets/Scripts/ToolTray.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTray : MonoBehaviour
+{
+    public GenericTool heldTool;
+    public bool isHoldingTool;
+
+    private float[] snapLocks = new float[7];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isHoldingTool = false;
+        SetSnapLocks();
+    }
+
+    /// <summary>
+    /// Sets currently held tool
+    /// </summary>
+    /// <param name="tool">GenericTool object that the player is holding; should be "this"</param>
+    public void SetHeldTool(GenericTool tool)
+    {
+        isHoldingTool = true;
+        heldTool = tool;
+    }
+
+    /// <summary>
+    /// Makes the toolTray forget which tool was being held when the player releases it
+    /// </summary>
+    public void UnsetHeldTool()
+    {
+        isHoldingTool = false;
+        heldTool = null;
+    }
+
+    /// <summary>
+    /// When the player's tool is hovering over the tool tray, left click will put the tool down
+    /// </summary>
+    public void SetPutDownState()
+    {
+        if (isHoldingTool)
+        {
+            heldTool.CommandSwitch(3);
+        }
+    }
+
+    /// <summary>
+    /// When the player's cursor leaves the tool tray, left click will no longer put the tool down
+    /// </summary>
+    public void UnsetPutdownState()
+    {
+        if (isHoldingTool)
+            heldTool.CommandSwitch(2);
+    }
+
+    /// <summary>
+    /// Returns the X value that the GenericTool can snap to
+    /// </summary>
+    /// <param name="i">the number of tool "slot", counting from the left</param>
+    public float GetSnapLock(int i)
+    {
+        return snapLocks[i];
+    }
+
+    /// <summary>
+    /// Sets locations for tool positions in accordance with screen size
+    /// </summary>
+    private void SetSnapLocks()
+    {
+        float snapLockStep = Screen.width / 8;
+        snapLocks[0] = snapLockStep * -3;
+        snapLocks[1] = snapLockStep * -2;
+        snapLocks[2] = snapLockStep * -1;
+        snapLocks[3] = 0;
+        snapLocks[4] = snapLockStep;
+        snapLocks[5] = snapLockStep * 2;
+        snapLocks[6] = snapLockStep * 3;
+
+        GenericTool[] toolsToLock = GetComponentsInChildren<GenericTool>();
+        foreach (GenericTool a in toolsToLock)
+        {
+            a.SetReturnPosition(snapLocks[a.slot]);
+        }
+    }
+}

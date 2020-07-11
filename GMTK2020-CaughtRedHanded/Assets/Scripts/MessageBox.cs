90589f546416c7e240cc47500b10bae304bd0f55
s15050 <45424170+s15050@users.noreply.github.com>
2020-07-11 18:27:10 +0200

    Adding a summonable message box
    Message box can be summoned with M

---
+++ GMTK2020-CaughtRedHanded/Assets/Scripts/MessageBox.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageBox : MonoBehaviour
+{
+    public Text message;
+    public Text buttonText;
+    public MessageCommanding messageCommander;
+
+    /// <summary>
+    /// Dismisses the message box
+    /// </summary>
+    public void DismissMessage()
+    {
+        messageCommander.DismissMessage();
+    }
+
+
+}
---
+++ GMTK2020-CaughtRedHanded/Assets/Scripts/MessageCommanding.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MessageCommanding : MonoBehaviour
+{
+    public GameObject messageBoxPrefab;
+    public Canvas canvas;
+    private GameObject messageBoxInstance;
+    private bool messageIsVisible;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        messageIsVisible = false;
+    }
+
+    /// <summary>
+    /// Summons the message box when M is pressed
+    /// </summary>
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M) && (!messageIsVisible))
+        {
+            messageBoxInstance = Instantiate(messageBoxPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+            messageBoxInstance.transform.parent = canvas.transform;
+            messageBoxInstance.transform.localPosition = new Vector3(0, 0, 0);
+            MessageBox messageBoxData = messageBoxInstance.GetComponent<MessageBox>();
+            messageBoxData.messageCommander = this;
+
+            messageIsVisible = true;
+        }
+    }
+
+    /// <summary>
+    /// Destroys the message window
+    /// </summary>
+    public void DismissMessage()
+    {
+        Destroy(messageBoxInstance);
+        messageBoxInstance = null;
+        messageIsVisible = false;
+    }
+}

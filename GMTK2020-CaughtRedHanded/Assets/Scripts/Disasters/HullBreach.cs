a39861bb11d04c9e5d0c58de86763fae162a58eb
ceje <cdone245@gmail.com>
2020-07-11 10:28:17 -0500

    Add hull breaches.

---
+++ GMTK2020-CaughtRedHanded/Assets/Scripts/Disasters/HullBreach.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HullBreach : Problem
+{
+    private bool fading = false;
+
+    private float fade = 1;
+    // Start is called before the first frame update
+    void Start(){
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (fading){
+            var rend = GetComponent<SpriteRenderer>();
+            var color = rend.color;
+            color.a -= Time.deltaTime * fade;
+            if (color.a <= 0){
+                DespawnMe();
+            }
+            rend.color = color;
+        }
+    }
+
+    private void DespawnMe(){
+        GlobalData.hullBreaches--;
+        Destroy(gameObject);
+    }
+
+    public override void Repair(){
+        var rend = GetComponent<SpriteRenderer>();
+        rend.color = Color.green;
+        fading = true;
+    }
+}

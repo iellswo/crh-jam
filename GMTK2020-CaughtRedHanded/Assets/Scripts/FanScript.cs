c4f32e82796026de25940d7fd8357e20a97d8e55
Ike Ellsworth <isaac@kina.net>
2020-07-11 13:17:28 -0600

    Fan sparks
    - Updated the particles on the fan.
    - Fan now spins back and forth when broken.
    - increased zone of repair
    - sparks should always appear in the center

--- GMTK2020-CaughtRedHanded/Assets/Scripts/FanScript.cs
+++ GMTK2020-CaughtRedHanded/Assets/Scripts/FanScript.cs
@@ -6,23 +6,48 @@ public class FanScript : MonoBehaviour
 {
     private Rigidbody2D _rigidbody2D;
 
-    public float fanSpeed;
+    public float fanSpeed = 120;
+    public float brokenSpeed = 60;
+
+    private float forward = 0.3f;
+    private float back = 0.1f;
+    private float timeInState;
+    private bool busted;
     // Start is called before the first frame update
     void Start(){
         _rigidbody2D = GetComponent<Rigidbody2D>();
         _rigidbody2D.angularVelocity = fanSpeed;
+        busted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (busted)
+        {
+            timeInState += Time.deltaTime;
+            if (timeInState >= back + forward)
+            {
+                timeInState = 0.0f;
+                _rigidbody2D.angularVelocity = brokenSpeed * -3;
+            }
+            else if (timeInState >= back)
+            {
+                _rigidbody2D.angularVelocity = brokenSpeed;
+            }
+        }
     }
 
-    public void Stop(){
-        _rigidbody2D.angularVelocity = 0;
+    public void Stop()
+    {
+        _rigidbody2D.angularVelocity = brokenSpeed * -3;
+        timeInState = 0.0f;
+        busted = true;
     }
 
-    public void Spin(){
+    public void Spin()
+    {
         _rigidbody2D.angularVelocity = fanSpeed;
+        busted = false;
     }
 }

d9ea2676475a463c5ab2d529207d5bb4b0056e52
ceje <cdone245@gmail.com>
2020-07-11 18:53:53 -0500

    The goose is loose

---
+++ GMTK2020-CaughtRedHanded/Assets/Scripts/Goose.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Goose : MonoBehaviour
+{
+    private int direction = 1;
+
+    public float speed = 20;
+    public float maxWidth = 10;
+    public float minInterval;
+    public float maxInterval;
+    public float jumpForce;
+    
+    private float interval;
+    private Rigidbody2D _rigidbody2D;
+    private SpriteRenderer _spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start(){
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _rigidbody2D.velocity = Vector2.right * speed;
+        interval = Random.Range(minInterval, maxInterval);
+    }
+
+    // Update is called once per frame
+    void Update(){
+        var velX = speed * direction;
+        var velY = _rigidbody2D.velocity.y;
+        if (Math.Abs(transform.position.x) >= maxWidth){
+            _rigidbody2D.transform.position = new Vector3((maxWidth-0.5f) * direction, transform.position.y, 0);
+            _spriteRenderer.flipX = !_spriteRenderer.flipX;
+            direction *= -1;
+        }
+
+        if (interval <= 0){
+            velY += jumpForce;
+            interval = Random.Range(minInterval, maxInterval);
+        }
+        _rigidbody2D.velocity = new Vector2(velX, velY);
+        interval -= Time.deltaTime;
+    }
+}

a1cd0f0b381eb485fdd0e660406e2ace9f3f70a7
ceje <cdone245@gmail.com>
2020-07-11 19:21:21 -0500

    Infinite Goose

---
+++ GMTK2020-CaughtRedHanded/Assets/Scripts/Disasters/Spawners/GooseGun.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GooseGun : MonoBehaviour
+{
+    public float minInterval;
+    public float maxInterval;
+    public Goose goose;
+
+    private float interval;
+    // Start is called before the first frame update
+    void Start(){
+        interval = Random.Range(minInterval, maxInterval);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (interval <= 0){
+            Instantiate(goose, transform.position, Quaternion.identity);
+            interval = Random.Range(minInterval, maxInterval);
+        }
+
+        interval -= Time.deltaTime;
+    }
+}
